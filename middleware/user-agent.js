586ce812bcb8c9d66a76ea64b3c478837533ea03
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2021-01-01 18:11:38 +0700

    fix: remove user agent

--- middleware/user-agent.js
+++ middleware/user-agent.js
@@ -2,6 +2,5 @@ export default function (context) {
   const userAgent = process.server
     ? context.req.headers['user-agent']
     : navigator.userAgent
-  console.log(userAgent)
   context.$auth.$storage.setCookie('userAgent', userAgent)
 }
